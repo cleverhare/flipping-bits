0d5cbd4b477a3211b0371c8ee924890e20e23b97
Soumyajit Brahma <soumyajitbrah@gmail.com>
2022-08-20 13:54:33 +0530

    sollution added

---
+++ app.js
@@ -0,0 +1,7 @@
+// Its the easiest hackerank problem. Just copy and paste the below code. HAAPY CODING 
+function flippingBits(n) {
+    // Write your code here
+       return ~n >>> 0;
+}
+
+//DONE
